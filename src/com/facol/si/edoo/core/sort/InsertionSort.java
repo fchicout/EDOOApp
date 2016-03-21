bba003020294bae6d6e56b06643ea713783195a7
Fabio Chicout <fchicout@gmail.com>
2016-03-20 21:44:52 -0300

    sort and sortStep implemented for other types

--- src/com/facol/si/edoo/core/sort/InsertionSort.java
+++ src/com/facol/si/edoo/core/sort/InsertionSort.java
@@ -7,55 +7,129 @@ public class InsertionSort implements ISortAlgorithm {
 		int temp, j;
 		for (int i = 1; i < data.length; i++) {
 			temp = data[i];
-			j=0;
-			for (j = i-1; j >= 0 && temp < data[j]; j--) {
-				data[j+1]=data[j];
+			j = 0;
+			for (j = i - 1; j >= 0 && temp < data[j]; j--) {
+				data[j + 1] = data[j];
 			}
-			data[j+1]=temp;
+			data[j + 1] = temp;
 		}
 		return data;
 	}
 
 	@Override
 	public int[] sortStep(int[] data, int step) {
-		// TODO Auto-generated method stub
-		return null;
+		int temp, j;
+		for (int i = 1; i < data.length; i++) {
+			temp = data[i];
+			j = 0;
+			for (j = i - 1; j >= 0 && temp < data[j]; j--) {
+				data[j + 1] = data[j];
+			}
+			data[j + 1] = temp;
+			if ((i * j) == step) {
+				return data;
+			}
+		}
+		return data;
 	}
 
 	@Override
 	public float[] sort(float[] data) {
-		// TODO Auto-generated method stub
-		return null;
+		float temp;
+		int j;
+		for (int i = 1; i < data.length; i++) {
+			temp = data[i];
+			j = 0;
+			for (j = i - 1; j >= 0 && temp < data[j]; j--) {
+				data[j + 1] = data[j];
+			}
+			data[j + 1] = temp;
+		}
+		return data;
 	}
 
 	@Override
 	public float[] sortStep(float[] data, int step) {
-		// TODO Auto-generated method stub
-		return null;
+		float temp;
+		int j;
+		for (int i = 1; i < data.length; i++) {
+			temp = data[i];
+			j = 0;
+			for (j = i - 1; j >= 0 && temp < data[j]; j--) {
+				data[j + 1] = data[j];
+			}
+			data[j + 1] = temp;
+			if ((i * j) == step) {
+				return data;
+			}
+		}
+		return data;
 	}
 
 	@Override
 	public double[] sort(double[] data) {
-		// TODO Auto-generated method stub
-		return null;
+		double temp;
+		int j;
+		for (int i = 1; i < data.length; i++) {
+			temp = data[i];
+			j = 0;
+			for (j = i - 1; j >= 0 && temp < data[j]; j--) {
+				data[j + 1] = data[j];
+			}
+			data[j + 1] = temp;
+		}
+		return data;
 	}
 
 	@Override
 	public double[] sortStep(double[] data, int step) {
-		// TODO Auto-generated method stub
-		return null;
+		double temp;
+		int j;
+		for (int i = 1; i < data.length; i++) {
+			temp = data[i];
+			j = 0;
+			for (j = i - 1; j >= 0 && temp < data[j]; j--) {
+				data[j + 1] = data[j];
+			}
+			data[j + 1] = temp;
+			if ((i * j) == step) {
+				return data;
+			}
+		}
+		return data;
 	}
 
 	@Override
 	public char[] sort(char[] data) {
-		// TODO Auto-generated method stub
-		return null;
+		char temp;
+		int j;
+		for (int i = 1; i < data.length; i++) {
+			temp = data[i];
+			j = 0;
+			for (j = i - 1; j >= 0 && temp < data[j]; j--) {
+				data[j + 1] = data[j];
+			}
+			data[j + 1] = temp;
+		}
+		return data;
 	}
 
 	@Override
 	public char[] sortStep(char[] data, int step) {
-		// TODO Auto-generated method stub
-		return null;
+		char temp;
+		int j;
+		for (int i = 1; i < data.length; i++) {
+			temp = data[i];
+			j = 0;
+			for (j = i - 1; j >= 0 && temp < data[j]; j--) {
+				data[j + 1] = data[j];
+			}
+			data[j + 1] = temp;
+			if ((i * j) == step) {
+				return data;
+			}
+		}
+		return data;
 	}
 
 	@Override
@@ -69,5 +143,5 @@ public class InsertionSort implements ISortAlgorithm {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 }
--- src/com/facol/si/edoo/core/sort/SelectionSort.java
+++ src/com/facol/si/edoo/core/sort/SelectionSort.java
@@ -22,14 +22,42 @@ public class SelectionSort implements ISortAlgorithm {
 
 	@Override
 	public int[] sortStep(int[] data, int step) {
-		// TODO Auto-generated method stub
-		return null;
+		for (int fixo = 0; fixo < data.length - 1; fixo++) {
+			int menor = fixo;
+			int i=menor;
+			for (; i < data.length; i++) {
+				if (data[i] < data[menor]) {
+					menor = i;
+				}
+			}
+			if (menor != fixo) {
+				int t = data[fixo];
+				data[fixo] = data[menor];
+				data[menor] = t;
+			}
+			if (fixo*i) == step){
+				return data;
+			}
+		}
+		return data;
 	}
 
 	@Override
 	public float[] sort(float[] data) {
-		// TODO Auto-generated method stub
-		return null;
+		for (int fixo = 0; fixo < data.length - 1; fixo++) {
+			int menor = fixo;
+			for (int i = menor; i < data.length; i++) {
+				if (data[i] < data[menor]) {
+					menor = i;
+				}
+			}
+			if (menor != fixo) {
+				float t = data[fixo];
+				data[fixo] = data[menor];
+				data[menor] = t;
+			}
+		}
+		return data;
 	}
 
 	@Override
@@ -40,8 +68,20 @@ public class SelectionSort implements ISortAlgorithm {
 
 	@Override
 	public double[] sort(double[] data) {
-		// TODO Auto-generated method stub
-		return null;
+		for (int fixo = 0; fixo < data.length - 1; fixo++) {
+			int menor = fixo;
+			for (int i = menor; i < data.length; i++) {
+				if (data[i] < data[menor]) {
+					menor = i;
+				}
+			}
+			if (menor != fixo) {
+				double t = data[fixo];
+				data[fixo] = data[menor];
+				data[menor] = t;
+			}
+		}
+		return data;
 	}
 
 	@Override
@@ -52,8 +92,20 @@ public class SelectionSort implements ISortAlgorithm {
 
 	@Override
 	public char[] sort(char[] data) {
-		// TODO Auto-generated method stub
-		return null;
+		for (int fixo = 0; fixo < data.length - 1; fixo++) {
+			int menor = fixo;
+			for (int i = menor; i < data.length; i++) {
+				if (data[i] < data[menor]) {
+					menor = i;
+				}
+			}
+			if (menor != fixo) {
+				char t = data[fixo];
+				data[fixo] = data[menor];
+				data[menor] = t;
+			}
+		}
+		return data;
 	}
 
 	@Override
